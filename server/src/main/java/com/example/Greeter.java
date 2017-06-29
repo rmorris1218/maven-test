6652627a5f26695954192ee72195a4f32e6e4963
Rob Morris <rmorris1218@users.noreply.github.com>
2017-06-29 15:15:23 -0500

    Added package-info.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,12 +1,4 @@
 /**
-  * This is a package for greeting someone in the HelloWorld application
-  *
-  * @author Rob Morris
-  * @version 0.1
-  */
-package com.example;
-
-/**
  * This is a class.
  */
 public class Greeter {
