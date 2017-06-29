ae9270ed9bba97e0189b5f15d2aa878dcc64a01c
Rob Morris <rmorris1218@users.noreply.github.com>
2017-06-29 15:05:09 -0500

    added javadoc comment and finalized param

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,14 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  
+  /**
+  * This is a method that is used to generate a greeting for someone
+  * visiting the page
+  * @param someone This is the name of the person visiting the package
+  * @return String This is the full greeting message
+  */
+  public String greet(final String someone) {
     return String.format("Hello, %s! Built by maven :)", someone);
   }
 }
