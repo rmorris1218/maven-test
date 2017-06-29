212d862a8925f8228bdd64c753208deb4cfa046b
Rob Morris <rmorris1218@users.noreply.github.com>
2017-06-29 15:13:24 -0500

    Fixed checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,3 +1,9 @@
+/**
+  * This is a package for greeting someone in the HelloWorld application
+  *
+  * @author Rob Morris
+  * @version 0.1
+  */
 package com.example;
 
 /**
@@ -11,14 +17,13 @@ public class Greeter {
   public Greeter() {
 
   }
-  
   /**
   * This is a method that is used to generate a greeting for someone
-  * visiting the page
+  * visiting the page.
   * @param someone This is the name of the person visiting the package
   * @return String This is the full greeting message
   */
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s! Built by maven :)", someone);
   }
 }
